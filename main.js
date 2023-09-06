2984139430f444aff7eff1c5a8554a167148a775
jetzable <gjetzable@gmail.com>
2023-09-05 22:17:50 -0600

    (home): First try with neumorphism.

--- main.js
+++ main.js
@@ -1,95 +1,32 @@
-particlesJS.load('particles-js', 'assets/particlesjs.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
-const scrollHandler = () => {
-
-  let menu = document.querySelector('.nav-bar')
-
-  let home = document.getElementById('home')
-  let aboutme = document.getElementById('aboutme')
-  let experience = document.getElementById('experience')
-  let more = document.getElementById('more')
-
-  let pos_nav_bar = window.pageYOffset + menu.offsetHeight
-
-  let pos_home = home.offsetTop + home.offsetHeight
-  let pos_aboutme = aboutme.offsetTop + aboutme.offsetHeight
-  let pos_experience = experience.offsetTop + experience.offsetHeight
-  let pos_more = more.offsetTop + more.offsetHeight
+  import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm';
   
-  let distance_home = pos_home - pos_nav_bar
-  let distance_aboutme = pos_aboutme - pos_nav_bar
-  let distance_experience = pos_experience - pos_nav_bar
-  let distance_more = pos_more - pos_nav_bar
+  const githubReposCount = document.getElementById('github-repos-count');
+  const tiktokFollowersCount = document.getElementById('tiktok-followers-count');
 
-  let min = Math.min(...[distance_home, distance_aboutme, distance_experience, distance_more].filter(num => num > 0))
+  emailjs.init('2OSzXwaSax4Sr2wJV');
 
-  document.querySelectorAll('.nav-bar .item')[0].classList.remove('highlight')
-  document.querySelectorAll('.nav-bar .item')[1].classList.remove('highlight')
-  document.querySelectorAll('.nav-bar .item')[2].classList.remove('highlight')
-  document.querySelectorAll('.nav-bar .item')[3].classList.remove('highlight')
+  async function getGithubInfo(username) {
+    const url = `https://api.github.com/users/${username}`;
+    const { data } = await axios.get(url);
+    console.log(data);
+    githubReposCount.innerHTML = data.public_repos;
+  };
 
-  if(min === distance_home) document.querySelectorAll('.nav-bar .item')[0].classList.add('highlight')
-  if(min === distance_aboutme) document.querySelectorAll('.nav-bar .item')[1].classList.add('highlight')
-  if(min === distance_experience) document.querySelectorAll('.nav-bar .item')[2].classList.add('highlight')
-  if(min === distance_more) document.querySelectorAll('.nav-bar .item')[3].classList.add('highlight')
+  async function getTiktokInfo(username) {
+    const url = `https://www.tiktok.com/v2/${username}/info`;
+    const resp = await axios({
+      method: 'get',
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        "fields": ["followerCount"]
+      }
+    });
+    console.log(resp);
+    tiktokFollowersCount.innerHTML = resp.data;
+  };
 
-}
-
-window.addEventListener('scroll', scrollHandler)
-
-// Wrap every letter in a span
-const textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml11 .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 500
-  })
-  .add({
-    targets: '.ml11 .line',
-    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-    easing: "easeOutExpo",
-    duration: 500,
-    delay: 100
-  }).add({
-    targets: '.ml11 .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 500,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i+1)
-  }).add({
-    targets: '.ml11',
-    opacity: 0,
-    duration: 3000,
-    easing: "easeOutExpo",
-    delay: 700
-  });
-
-  // Wrap every letter in a span
-const nameWrapper = document.querySelector('.ml12');
-nameWrapper.innerHTML = nameWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml12 .letter',
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: (el, i) => 500 + 30 * i
-  }).add({
-    targets: '.ml12 .letter',
-    translateX: [0,-30],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 600,
-    delay: (el, i) => 100 + 30 * i
-  });
+  // getGithubInfo('jetzable');
+  // getTiktokInfo('lababito');
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,18 +24,29 @@ module.exports = {
       '36': '9rem',
     },
     extend: {
+      backgroundImage: {
+        'profile': "url('/assets/images/palm.jpg')"
+      },
+      boxShadow: {
+        'neumorph': '-4px -4px 8px #d9d7d5, 4px 4px 5px #ffffff',
+        'neuhover': '-2px -2px 6px rgba(255, 255, 255, .6), -2px -2px 4px rgba(255, 255, 255, .4), 2px 2px 2px rgba(255, 255, 255, .05), 2px 2px 4px rgba(0, 0, 0, .1)',
+        'neuactive': 'inset -2px -2px 6px rgba(255, 255, 255, .7), inset -2px -2px 4px rgba(255, 255, 255, .5), inset 2px 2px 2px rgba(255, 255, 255, .075), inset 2px 2px 4px rgba(0, 0, 0, .15)'
+      },
       colors: {
+        'neumorph': '#efefef',
+        'secondary': '#48BF84',
+        'secondary-end': '#DE4D86',
         'body': '#457B9D',
-        'card': '#EDFAFD',
+        'card': '#efefef',
         'body-end': '#A8DADC',
         'selected-text': '#ff5f5f',
-        'main': '#1D3557',
+        'main': '#444',
         'nav': '#F1FAEE',
         'dot': '#20D5A8'
       },
       fontFamily: {
+        'nunito': ["'Nunito'", 'sans-serif'],
         'lato': ["'Lato'", 'sans-serif'],
-        'rationale': ["'Rationale'", 'sans-serif']
       }
     },
   },
